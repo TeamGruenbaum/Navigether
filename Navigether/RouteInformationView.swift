209a1c7f0ed750c428062ecfce310567d8d09607
isabellwaas <isabell.waas@hof-university.de>
2021-12-31 12:45:58 +0100

    Add Call Bus Alert

--- Navigether/RouteInformationView.swift
+++ Navigether/RouteInformationView.swift
@@ -4,6 +4,7 @@ struct RouteInformationView:View
 {
     var routeInformationIconNames:[String]
     var callBus:Bool
+    @State var callBusAlertDialogBox=false
     
     var body:some View
     {
@@ -22,7 +23,15 @@ struct RouteInformationView:View
             {
                 Button("Reservieren", action:
                 {
-                
+                    callBusAlertDialogBox.toggle()
+                })
+                .alert(isPresented: $callBusAlertDialogBox, content:
+                {
+                    
+                    let firstButton = Alert.Button.default(Text("Reservieren"))
+                    let secondButton = Alert.Button.destructive(Text("Abbrechen"))
+                     
+                    return Alert(title: Text("Anruflinienbus\nLinie 15: 21:07 - 21:58 Uhr"), message: Text("Leierndorfer Str., Langquaid\nnach\nRegensburg Hauptbahnhof, Regensburg"), primaryButton: firstButton, secondaryButton: secondButton)
                 })
                 .foregroundColor(Color(UIColor(named: "Secondary")!))
             }
